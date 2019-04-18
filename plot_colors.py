3af3acebb01921ec06dddcd361533076675d8b37
Patrick Shriwise <pshriwise@gmail.com>
2019-04-18 08:49:45 -0500

    Correction to rgb invert function.

--- plot_colors.py
+++ plot_colors.py
@@ -10,8 +10,7 @@ def random_rgb():
 def rgb_normalize(rgb):
     return tuple([c/255. for c in rgb])
 
-def invert_rgb(rgb, normalized = False):
+def invert_rgb(rgb, normalized=False):
     rgb_max = 1.0 if normalized else 255.
     inv = [rgb_max - c for c in rgb[0:3]]
     return (*inv, *rgb[3:])
-    return inv
