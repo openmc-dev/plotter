9ff32b70e59177a6e0efef0464692ee7c0aba5b8
Patrick Shriwise <pshriwise@gmail.com>
2019-11-02 09:36:02 -0500

    Separating out dock classes.

---
+++ common_widgets.py
@@ -0,0 +1,10 @@
+from PySide2.QtWidgets import QFrame
+
+
+
+
+class HorizontalLine(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
