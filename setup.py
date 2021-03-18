276d70f3110fbfc3465d52dbcad607bf8f6274d5
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-03-18 22:55:50 +0000

    version bump to avoid test pypi fail

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 kwargs = {
     'name': 'openmc-plotter',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'packages': ['openmc_plotter'],
     'package_data': {'openmc_plotter' : ['assets/*.png']},
     'entry_points': {
